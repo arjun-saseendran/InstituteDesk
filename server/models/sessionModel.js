3a32fe85b125fde745fa741781c21b4354ceffad
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-29 13:56:58 +0530

    add session schema

---
+++ server/models/sessionModel.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+const sessionSchema = new Schema(
+  {
+    title: String,
+    required: true,
+    miniLength: 3,
+    maxLength: 30,
+  },
+  {
+    startTime: String,
+    required: true,
+  },
+  {
+    endTime: String,
+    required: true,
+  },
+);
