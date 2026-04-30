5710781a3842c7113cc275f51b3b0620248dbb23
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-30 14:04:41 +0530

    add class routes

---
+++ server/routes/v1/classRoute.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { createClass, deleteClass, getClass, getClasses, UpdateClass } from "../../controllers/classController";
+
+
+
+// config router
+const classRouter = Router();
+
+// create class
+classRouter.post("/", createClass)
+
+// get all classes
+classRouter.get("/", getClasses)
+
+// get single class
+classRouter.get("/:id", getClass)
+
+// update class
+classRouter.put("/:id", UpdateClass)
+
+// delete class
+classRouter.delete("/:id", deleteClass)
--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { sessionRouter } from "./sessionRoute.js";
-
+import { classRouter } from "./classRoute.js";
 
 // config router
-export const v1Router = Router()
+export const v1Router = Router();
 
-v1Router.use("/session", sessionRouter)
+v1Router.use("/session", sessionRouter);
+v1Router.use("/class", classRouter);
