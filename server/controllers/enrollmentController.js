0f00059fff21bb24113d0e1a00b345f0636092a7
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-08 14:47:56 +0530

    add createEnrollment

---
+++ server/controllers/enrollmentController.js
@@ -0,0 +1,31 @@
+import { Enrollment } from "../models/enrollmentModel.js";
+
+
+// create enrollment
+export const createEnrollment = async (req, res) => {
+  try {
+    // data from request body
+    const { userId, classId, sessionId} = req.body;
+
+    // validate data
+    if (!userId || !classId || !sessionId) {
+      return res.status(400).json({ err: "all fields required" });
+    }
+
+    // create new enrollment object
+    const newEnrollment = new Enrollment({
+      userId,
+      classId,
+      sessionId,
+    });
+
+    // save enrollment to database
+    await newEnrollment.save();
+
+    // success response to client
+    res.status(201).json({ msg: "enrollment created successfully", data: newEnrollment });
+  } catch (error) {
+    // send error response to client
+    return res.status(500).json({ err: "failed to create enrollment" });
+  }
+};
